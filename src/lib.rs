15ef08e1c34bc6c4094855d10d276d8a5c39b7df
Quang M. Bui <minhquangbui053@gmail.com>
2025-05-28 07:52:38 +0000

    Refactor argument parsing and search functions
    - Updated  to use a double-ended iterator for argument parsing, improving flexibility and error handling.
    
    - Simplified  and  functions using iterator combinators for cleaner and more idiomatic Rust code.
    
    - Removed unnecessary collection of arguments in , directly passing the iterator to .
    
    Signed-off-by: Quang M. Bui <minhquangbui053@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -7,17 +7,25 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(
+        mut args: impl DoubleEndedIterator<Item = String>,
+    ) -> Result<Config, &'static str> {
+        args.next();
+        let mut args = args.rev();
 
-        let query = args[args.len() - 2].clone();
-        let file_path = args[args.len() - 1].clone();
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
 
         let mut ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        for arg in args[1..args.len() - 2].iter() {
+        for arg in args {
             if arg.eq("--ignore-case") {
                 ignore_case = true;
             }
@@ -48,28 +56,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
